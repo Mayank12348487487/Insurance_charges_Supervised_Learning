5c2b4756c36b6fe2f227c8e6033cdc527cba514b
Mayank12348487487 <136908056+Mayank12348487487@users.noreply.github.com>
2026-02-21 09:11:48 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -97,7 +97,7 @@ export default function App() {
     }
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/predict", {
+      const res = await fetch("https://insurancechargespredictor.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
